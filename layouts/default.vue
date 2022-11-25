917b1d0135fb03b4ba92ff8f9be038cc3957a11d
xouayang <79002013+xouayang@users.noreply.github.com>
2022-11-25 18:41:55 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>       
+        <v-app-bar :clipped-left="clipped" fixed app color="primary">
+          <v-toolbar-title>{{ title }}</v-toolbar-title>
+          <v-spacer />
+        </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
